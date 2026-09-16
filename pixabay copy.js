bd1431dd82acf752a612995afee203809f3db6c0
Marina Cordeiro <cordeiromarina737@gmail.com>
2026-09-16 15:17:38 -0300

    feat: organiza a estrutura das imagens

--- pixabay copy.js
+++ pixabay copy.js
@@ -1,14 +1,15 @@
 "use strict";
+
 const searchImages = async (text) => {
-  const key = "57606088-0b7544a690264f460cd57a29b";
+  const key = "12180485-63058e50c2824559402449f76";
   const url = `https://pixabay.com/api/?key=${key}&q=${text}`;
   const response = await fetch(url);
   return response.json();
-  //estou pegando apenas o json
 };
+
 const createLink = (tag) => `
-    <a hrfe="#" onClick="loadGallery('${tag}')">
-    ${tag}
+    <a href="#" onClick="loadGallery('${tag}')">
+        ${tag}
     </a>
 `;
 
@@ -39,34 +40,60 @@ const createCard = ({ webformatURL, pageURL, tags, likes, comments }) => {
 
         </div>
     `;
-
   return card;
 };
 
 const loadGallery = async (text, page = 1) => {
   const container = document.querySelector(".container-gallery");
-  const { hits } = await searchImages(`${text}&page=${page}`);
+  const { hits, totalHits } = await searchImages(`${text}&page=${page}`);
   const cards = hits.map(createCard);
   container.replaceChildren(...cards);
-  document.querySelector("#search-input").value = text;
 
+  const totaPages = Math.ceil(totalHits / 20);
+  document.querySelector("#page-total").textContent = `/ ${totaPages}`;
   document.querySelector("#search-input").value = text;
   document.querySelector("#page").value = page;
 };
 
-//o target é onde eu estou cliando, ou seja, o valor
-const handleKeyPress = ({ key, target }) => {
+const handleKeypress = ({ key, target }) => {
   if (key === "Enter") {
     loadGallery(target.value);
   }
 };
 
-const handlePage= ({key, target}) =>{
-    const text = document.querySelector('#search-input')
-}
+const handlePage = ({ key, target }) => {
+  const text = document.querySelector("#search-input").value;
+  if (key === "Enter") {
+    loadGallery(text, target.value);
+  }
+};
 
-//keypress é quando alguém preciona uma tecla, keyup é quando a tecla é solta e keydown é quando a tecla é pressionada e segurada
+const handleNext = () => {
+  let page = Number(document.querySelector("#page").value);
+  const totalPages = Number(
+    document.querySelector("#page-total").textContent.replace("/", ""),
+  );
+  const text = document.querySelector("#search-input").value;
+  if (page < totalPages) {
+    page++;
+    loadGallery(text, page);
+  }
+};
+
+const handlePrevious = () => {
+  let page = Number(document.querySelector("#page").value);
+  const text = document.querySelector("#search-input").value;
+  if (page > 1) {
+    page--;
+    loadGallery(text, page);
+  }
+};
+
 document
   .querySelector("#search-input")
-  .addEventListener("keypress", handleKeyPress);
-document.querySelector('#page').addEventListener('keypress', handlePage)
+  .addEventListener("keypress", handleKeypress);
+document.querySelector("#page").addEventListener("keypress", handlePage);
+document.querySelector("#page-next").addEventListener("click", handleNext);
+document
+  .querySelector("#page-previous")
+  .addEventListener("click", handlePrevious);
